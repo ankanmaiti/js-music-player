3823871678d36e2ba6e12019e9533333b352a919
ankanmaiti <ankanmaiti2+github@gmail.com>
2023-08-14 11:26:01 +0530

    update previouse code feature - skip, loaded track

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
-const audioPlayer = document.getElementById('audio-player')
-const playStatus = document.querySelector('[data-play]')
+const audioElement = document.getElementById('audio-player')
+const togglePlayer = document.querySelector('[data-toggle-player]')
 const backwordPlay = document.querySelector('[data-backword]')
 const forwordPlay = document.querySelector('[data-forword]')
 const audioTrack = document.getElementById('audio-track')
 
+// Toggle play/pause on click
+togglePlayer.addEventListener('click', () => {
 
-playStatus.addEventListener('click', event => {
+    const icon = togglePlayer.firstElementChild
 
-    const icon = playStatus.firstElementChild
+    const isPlayingAfterToggle = toggleMediaPlayer(audioElement, audioElement.paused)
 
-    if (playStatus.dataset.play == "false") {
-        playStatus.dataset.play = "true"
-        audioPlayer.play()
+    icon.classList.toggle("fa-play", !isPlayingAfterToggle)
+    icon.classList.toggle("fa-pause", isPlayingAfterToggle)
 
-        icon.classList.replace("fa-play", "fa-pause")
-    } else {
-        audioPlayer.pause()
-        playStatus.dataset.play = "false"
-
-        icon.classList.replace("fa-pause", "fa-play")
-    }
 })
 
+// Update progress bar as audio plays
+audioElement.addEventListener('timeupdate', ({ target:player }) => {
+    const audioTime = player.currentTime / player.duration * 100
+    audioTrack.style.setProperty('--audio-played', `${audioTime}%`)
+})
 
-audioPlayer.addEventListener('timeupdate', event => {
-    const audioTime = audioPlayer.currentTime / audioPlayer.duration * 100
-    audioTrack.style.setProperty('--handle-position', `${audioTime}%`)
+// Update buffer progress as audio loads
+audioElement.addEventListener('progress', ({ target:player }) => {
+    const loadedTrack = claculatBufferEnd( player )
+    audioTrack.style.setProperty('--audio-playable', `${loadedTrack}%`)
+})
+
+// Calculate buffered progress percentage
+function claculatBufferEnd( player ) {
+    const buffered = player.buffered
+    const bufferedLength = buffered.length
+    if (bufferedLength <= 0) return
+
+    const lastEndTime = buffered.end(bufferedLength - 1)
+    const loadedTrack = lastEndTime / player.duration * 100
+
+    return loadedTrack
+}
+
+// Toggle play/pause of the media player
+function toggleMediaPlayer(playerElement, paused = true) {
+    if (paused) {
+        playerElement.play()
+        return true
+    }
+
+    playerElement.pause()
+    return false
+}
+
+// Skip forward by 10 seconds
+forwordPlay.addEventListener('click', ()=> {
+    audioElement.currentTime += 10
+})
+
+// Skip backward by 10 seconds
+backwordPlay.addEventListener('click', ()=> {
+    audioElement.currentTime -= 10
 })
